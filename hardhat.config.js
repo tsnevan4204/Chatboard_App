161221d4c439abf786f2c8bce48955b8633b11e2
tsnevan4204 <98066843+tsnevan4204@users.noreply.github.com>
2025-02-18 18:59:35 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/kls8f86lGR1T0YeE-_mGfYiYMcfgKzik",
-      accounts: ["5f49b9f8c758b463b5e1bfc1ee366a7c2f25fc045b552229cdc509e47f9d2ca5"],
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
     },
   }
 };
